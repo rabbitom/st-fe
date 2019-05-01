6470e032af5a7e8d50c7f30a372ad07c925348e2
erabbit <rabbitom@gmail.com>
2019-05-01 17:02:32 +0800

    feat: 实现填写URL的表单并在确定时跳转

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <URLForm/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import URLForm from './components/URLForm.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    URLForm
   }
 }
 </script>
---
+++ src/components/URLForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <el-form class="url-form">
+      <el-form-item label="网址">
+        <el-input v-model="url"></el-input>
+      </el-form-item>
+      <el-form-item label="格式">
+        <el-radio-group v-model="format">
+          <el-radio label="json">JSON</el-radio>
+          <el-radio label="text">文本</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="分割线">
+        <el-input :disabled="format !== 'text'" v-model="separator"></el-input>
+      </el-form-item>
+      <el-button @click="onConfirm">确定</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: '',
+      format: 'json',
+      separator: ''
+    }
+  },
+  methods: {
+    onConfirm() {
+      var url = `/api/thread/?url=${encodeURIComponent(this.url)}&format=${this.format}`
+      if(this.format === 'text')
+        url += `&separator=${this.separator}`
+      document.location = url
+    }
+  }
+}
+</script>
+
+<style scoped>
+.url-form {
+  max-width: 1024px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
