7b843420ac03a38004ed0e642cb10b6729176afa
erabbit <rabbitom@gmail.com>
2019-05-03 11:15:33 +0800

    feat: 调整参数

--- src/components/URLForm.vue
+++ src/components/URLForm.vue
@@ -4,14 +4,16 @@
       <el-form-item label="网址">
         <el-input v-model="url"></el-input>
       </el-form-item>
-      <el-form-item label="格式">
-        <el-radio-group v-model="format">
-          <el-radio label="json">JSON</el-radio>
+      <el-form-item label="模式">
+        <el-radio-group v-model="mode">
           <el-radio label="text">文本</el-radio>
+          <el-radio label="down">下载</el-radio>
+          <el-radio label="send">发送到笔记</el-radio>
+          <el-radio label="json">JSON</el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item label="分割线">
-        <el-input :disabled="format !== 'text'" v-model="separator"></el-input>
+        <el-input :disabled="mode === 'json'" v-model="separator"></el-input>
       </el-form-item>
       <el-button @click="onConfirm">确定</el-button>
     </el-form>
@@ -23,15 +25,13 @@ export default {
   data() {
     return {
       url: '',
-      format: 'json',
+      mode: 'text',
       separator: ''
     }
   },
   methods: {
     onConfirm() {
-      var url = `/api/thread/?url=${encodeURIComponent(this.url)}&format=${this.format}`
-      if(this.format === 'text')
-        url += `&separator=${this.separator}`
+      var url = `/api/thread/?url=${encodeURIComponent(this.url)}&mode=${this.mode}&separator=${this.separator}`
       document.location = url
     }
   }
